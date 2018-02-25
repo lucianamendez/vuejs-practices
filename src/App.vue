85f09494fcf8e27a193a1f6d3f426d2e2062577e
Luciana Mendez <dg.lucianamendez@gmail.com>
2018-02-25 11:28:58 -0300

    punch bag exercise

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,19 @@
 <template>
   <div id="app">
-    <img src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <vue-app />
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import VueApp from './components/VueApp.vue'
 
 export default {
   name: 'app',
   components: {
-    HelloWorld
+    VueApp
   }
 }
 </script>
 
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
 </style>
---
+++ src/components/VueApp.vue
@@ -0,0 +1,67 @@
+<template>
+ <div id="vue-app">
+    <div id="bag" v-bind:class="{ burst: ended }"></div>
+    <div id="bag-health">
+      <div v-bind:style="{ width: health + '%' }"></div>
+    </div>
+    <div id="controls">
+      <button v-on:click="punch" v-show="!ended">Punch</button>
+      <button v-on:click="restart">Restart</button>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+    name: 'VueApp',
+    data(){
+        return {
+        health: 100,
+        ended: false
+        }
+    },
+    methods: {
+        punch: function(){
+            this.health -= 10;
+            if ( this.health <= 0 ){
+                this.ended = true;
+            }
+        },
+        restart: function(){
+            this.health = 100;
+            this.ended = false;
+        }
+    }
+}
+</script>
+
+
+<style scoped>
+#bag{
+    width: 200px;
+    height: 500px;
+    margin: 0 auto;
+    background: url(../assets/img/bag.png) center no-repeat;
+    background-size: 80%;
+}
+
+#bag.burst{
+    background-image: url(../assets/img/bag-burst.png);
+}
+
+#bag-health{
+    width: 200px;
+    border: 2px solid #000;
+    margin: 0 auto 20px auto;
+}
+
+#bag-health div{
+    height: 20px;
+    background: crimson
+}
+
+#controls{
+    width: 120px;
+    margin: 0 auto;
+}
+</style>
